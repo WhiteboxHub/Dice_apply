359dd8a0e4312a48a2fc5a154a46f996ebd10b70
shivapatel7 <shivapatel098k@gmail.com>
2024-07-31 22:55:19 +0530

    tsconfig

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx}",
     baseUrl: "https://www.dice.com/",
     includeShadowDom: true,
     pageLoadTimeout: 100000,
